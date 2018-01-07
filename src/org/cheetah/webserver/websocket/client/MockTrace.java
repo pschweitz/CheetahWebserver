11ecac6fd28127208ec0116b159ba01aac2098b4
Philippe Schweitzer <pschweitz@orange.fr>
2018-01-07 18:05:18 +0100

    cleanup

--- src/org/cheetah/webserver/websocket/client/MockTrace.java
+++
@@ -1,8 +0,0 @@
-package org.cheetah.webserver.websocket.client;
-
-import org.simpleframework.transport.trace.Trace;
-
-public class MockTrace implements Trace{
-   public void trace(Object event) {}
-   public void trace(Object event, Object value) {}
-}
